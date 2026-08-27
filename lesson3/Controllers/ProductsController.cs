a99be694c56ebf1ad6a0d0cec6aaae8bf128315d
Mr Sigma <najdyuk.stas1@gmail.com>
2026-08-27 17:28:46 +0300

    Add project files.

---
+++ lesson3/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lesson3.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Product Page");
+        }
+        public IActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return Content("Invalid product ID");
+            }
+            else
+            {
+                return Content("Details Page : " + id);
+            }
+        }
+        public IActionResult About()
+        {
+            return Content("About Page");
+        }
+        public IActionResult Search(int id, string nameProduct, int cost)
+        {
+            return Content($"Search Results - ID: {id}, Name: {nameProduct}, Cost: {cost}");
+        }
+    }
+}
---
+++ lesson3/Controllers/TimeManagerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lesson3.Controllers
+{
+    public class TimeManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Me()
+        {
+            return Content("Free Time: 10 hours");
+        }
+        public IActionResult AboutMe()
+        {
+            return Content("About Me \nMy name: John Doe \nMy age: 30 \nMy hobbies: Reading, Swimming");
+        }
+        public IActionResult ContactMe()
+        {
+            return Content("Contact Me \nEmail: john.doe@example.com \nPhone: 123-456-7890");
+        }
+        public IActionResult ViewName()
+        {
+            return Content("My name is John Doe");
+        }
+        public IActionResult ViewLastName()
+        {
+            return Content("My last name is Doe");
+        }
+        public IActionResult ManageTime(string task, int hours)
+        {
+            if(hours <= 0 || task == null || task == "")
+            {
+                return Content("Error: Invalid input");
+            }
+            return Content($"Task: {task} \nHours for it: {hours}");
+        }
+    }
+}
